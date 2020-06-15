66f6b6a6a7c64ae225cd3010cf51a2a9b49f2589
Saad-Noor <saadnoor711@gmail.com>
2020-06-15 02:51:20 +0200

    show contract list from array

--- src/app/contract-list/contract-list.component.ts
+++ src/app/contract-list/contract-list.component.ts
@@ -12,6 +12,140 @@ export class ContractListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    this.contractList = [
+      {
+        "name": "Customer",
+        "address": "Customer Address",
+        "totalPrice": 38,
+        "brokerName": "Broker Name",
+        "brokerAddress": "Broker Address",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+    
+      },
+      {
+        "name": "Customer1",
+        "address": "Customer Address1",
+        "totalPrice": 10,
+        "brokerName": "Broker Name1",
+        "brokerAddress": "Broker Address1",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      },
+      {
+        "name": "Customer1",
+        "address": "Customer Address1",
+        "totalPrice": 10,
+        "brokerName": "Broker Name1",
+        "brokerAddress": "Broker Address1",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      },
+      {
+        "name": "Customer1",
+        "address": "Customer Address1",
+        "totalPrice": 10,
+        "brokerName": "Broker Name1",
+        "brokerAddress": "Broker Address1",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      },
+      {
+        "name": "Customer1",
+        "address": "Customer Address1",
+        "totalPrice": 10,
+        "brokerName": "Broker Name1",
+        "brokerAddress": "Broker Address1",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      },
+      {
+        "name": "Customer2",
+        "address": "Customer Address2",
+        "totalPrice": 20,
+        "brokerName": "Broker Name2",
+        "brokerAddress": "Broker Address2",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+    
+      },
+      {
+        "name": "Customer3",
+        "address": "Customer Address3",
+        "totalPrice": 32,
+        "brokerName": "Broker Name3",
+        "brokerAddress": "Broker Address3",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      },
+      {
+        "name": "Customer",
+        "address": "Customer Address",
+        "totalPrice": 38,
+        "brokerName": "Broker Name",
+        "brokerAddress": "Broker Address",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+    
+      },
+      {
+        "name": "Customer1",
+        "address": "Customer Address1",
+        "totalPrice": 10,
+        "brokerName": "Broker Name1",
+        "brokerAddress": "Broker Address1",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      },
+      {
+        "name": "Customer1",
+        "address": "Customer Address1",
+        "totalPrice": 10,
+        "brokerName": "Broker Name1",
+        "brokerAddress": "Broker Address1",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      },
+      {
+        "name": "Customer1",
+        "address": "Customer Address1",
+        "totalPrice": 10,
+        "brokerName": "Broker Name1",
+        "brokerAddress": "Broker Address1",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      },
+      {
+        "name": "Customer1",
+        "address": "Customer Address1",
+        "totalPrice": 10,
+        "brokerName": "Broker Name1",
+        "brokerAddress": "Broker Address1",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      },
+      {
+        "name": "Customer2",
+        "address": "Customer Address2",
+        "totalPrice": 20,
+        "brokerName": "Broker Name2",
+        "brokerAddress": "Broker Address2",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+    
+      },
+      {
+        "name": "Customer3",
+        "address": "Customer Address3",
+        "totalPrice": 32,
+        "brokerName": "Broker Name3",
+        "brokerAddress": "Broker Address3",
+        "contractStartDate": new Date(),
+        "contractEndDate": new Date()
+      }
+    ]
+    
   }
 
 }
