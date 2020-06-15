a176db17f43c51a12a08e5c41d0fe6d73fc8ef1d
Saad-Noor <saadnoor711@gmail.com>
2020-06-15 02:46:57 +0200

    added toolbar and fab button

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NgModalImp
+# Angular 9 Finlex Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.1.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AngularMaterialModule } from './angular-material.module'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContractListComponent } from './contract-list/contract-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContractListComponent
   ],
   imports: [
     BrowserModule,
